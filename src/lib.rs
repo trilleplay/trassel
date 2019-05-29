86dfd201abb9c9fbb10b863dd552df2053e2ea3f
Trilleplay <tristan@farkas.st>
2019-05-29 13:00:52 +0200

    This is probably a better idea then using rng proccess ids.

--- src/lib.rs
+++ src/lib.rs
@@ -10,19 +10,15 @@ pub mod generate {
         response
     }
     #[no_mangle]
-    pub fn gen_id(server_id: i32, random_process_id: i32) -> u64 {
-        if (random_process_id > 1000) {
-            panic!("The Random Process ID can not be above 1000.");
-        }
+    pub fn gen_id(server_id: i32) -> u64 {
         if (server_id * 3 > random_process_id) {
             panic!("The serverid times three can not be larger then the process id.");
         }
         let serverid = server_id as u64;
-        let randomprocessid = random_process_id as u64;
         // 05/29/2019 @ 7:55am (UTC)
         let custom_epoch = self::base_calc() as u64 - 15591165000000000;
         let epoch_scale = custom_epoch * 1000;
-        let id = epoch_scale + randomprocessid - serverid * 3;
+        let id = epoch_scale + serverid * serverid * 3;
         std::thread::sleep(3*durations::MILLISECOND);
         id
     }
